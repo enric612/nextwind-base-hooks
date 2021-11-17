9db7a27ab0dcbd1168a9b0527b04135434579852
Enric Climent <enric612@gmail.com>
2021-11-17 15:48:14 +0100

    useState basic

---
+++ pages/hooks/usestate.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+function useStatePage() {
+    
+    const [count, setCount] = useState(4);
+
+    // Decrement Count function
+    function decrementCount(){
+        // Not perfect way
+        // setCount(count - 1);
+        // Recomended way
+        setCount(prevCount => prevCount - 1);
+    }
+
+    // Increment Coutn Function
+    function incrementCount(){
+         // Not perfect way
+        // setCount(count + 1);
+        // Recomended way
+        setCount(prevCount => prevCount + 1);
+    }
+
+    return (
+        <>
+            <div className="flex flex-row items-center justify-center w-full flex-1 px-20 text-center pt-20">
+                <button className="btn text-2xl" onClick={decrementCount}>-</button>
+                <span className="text-2xl mx-4">{count}</span>
+                <button className="btn text-2xl" onClick={incrementCount}>+</button>
+            </div>
+        </>
+    )
+}
+
+export default useStatePage;
