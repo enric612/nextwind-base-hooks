1ebeaaf506d971f94a61cb0e3504fdc4e5b4ed80
Enric Climent <enric612@gmail.com>
2021-11-21 13:17:23 +0100

    use memo

--- pages/hooks/usememo.js
+++ pages/hooks/usememo.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
-export default function useMemoPage(){
+export default function useMemoPage() {
     const [number, setNumber] = useState(0);
     const [dark, setDark] = useState(false);
-    const doubleNumber = slowFunction(number);
+    const doubleNumber = useMemo(() => { 
+        return slowFunction(number) 
+    }, [number]);
     const themeStyles = {
-        backgroundColor: dark? 'black': 'white',
-        color: dark? 'white':'black'
+        backgroundColor: dark ? 'black' : 'white',
+        color: dark ? 'white' : 'black'
     }
 
-    return(
+    useEffect(() => {
+        console.log("Theme Changed")
+    }, [themeStyles])
+
+    return (
         <div>
-                    
-            <input type="number" value={number} onChange={e=>setNumber(parseInt(e.target.value))}/>
-            <button className="btn" onClick={() => setDark(prevDark=>!prevDark)}> Change Theme</button>
+
+            <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+            <button className="btn" onClick={() => setDark(prevDark => !prevDark)}> Change Theme</button>
             <div className="mt-10 font-bold text-2xl text-center border-t" style={themeStyles}>{doubleNumber}</div>
         </div>
     )
@@ -21,6 +27,6 @@ export default function useMemoPage(){
 
 function slowFunction(num) {
     console.log('Calling Slow Function')
-    for (let i = 0; i<= 1000000000; i++){}
-    return num*2;
+    for (let i = 0; i <= 1000000000; i++) { }
+    return num * 2;
 }
