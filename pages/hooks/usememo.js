277bb93867e31646b4651994ea15a89190c51b87
Enric Climent <enric612@gmail.com>
2021-11-21 12:59:00 +0100

    use memo no memo

---
+++ pages/hooks/usememo.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default function useMemoPage(){
+    const [number, setNumber] = useState(0);
+    const [dark, setDark] = useState(false);
+    const doubleNumber = slowFunction(number);
+    const themeStyles = {
+        backgroundColor: dark? 'black': 'white',
+        color: dark? 'white':'black'
+    }
+
+    return(
+        <div>
+                    
+            <input type="number" value={number} onChange={e=>setNumber(parseInt(e.target.value))}/>
+            <button className="btn" onClick={() => setDark(prevDark=>!prevDark)}> Change Theme</button>
+            <div className="mt-10 font-bold text-2xl text-center border-t" style={themeStyles}>{doubleNumber}</div>
+        </div>
+    )
+}
+
+function slowFunction(num) {
+    console.log('Calling Slow Function')
+    for (let i = 0; i<= 1000000000; i++){}
+    return num*2;
+}
